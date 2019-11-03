c2b5e264f8840da1ea15b32620d0e7628362f8dd
MicahJank <MicahJank@users.noreply.github.com>
2019-11-03 17:50:24 -0600

    added redux boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import allReducers from './reducers';
+
+const enhancer = compose(applyMiddleware(thunk, logger)); // logger must allways be at the end of apply middleware
+const store = createStore(allReducers, enhancer);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
