682d569a2d52f8db681eac81d741c2a46a59ab0d
MicahJank <MicahJank@users.noreply.github.com>
2019-11-03 17:30:47 -0600

    added cors middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 
 const postsRouter = require('./posts/posts-router.js');
@@ -6,7 +7,7 @@ const postsRouter = require('./posts/posts-router.js');
 const server = express();
 
 server.use(express.json());
-
+server.use(cors());
 server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
